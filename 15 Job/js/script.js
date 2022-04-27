2087877c5b23184e11fa9b30147b28c77b9b6db9
murriae <abby.murri@gmail.com>
2022-04-27 12:02:25 -0400

    15

---
+++ 15 Job/js/script.js
@@ -0,0 +1,6 @@
+const hb = document.querySelector('#hamburgerBtn');
+const pn = document.querySelector('#primaryNav');
+hb.addEventListener('click', () => {
+    hb.classList.toggle('open');
+    pn.classList.toggle('open');
+})
